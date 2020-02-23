42cc66f960f94d8bed5f53028d260a7893bd2f00
TY-TOWER-PC\ty_tower_pc <tyler@codebycandle.com>
2020-02-22 19:29:54 -0800

    adding class to handle natural-docs docu plugin

---
+++ scripts/docu/NaturalDocsTestScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class: NaturalDocsTestScript
+// Demo script to test out the Unity plugin _Natural Docs_ for project docu.
+public class NaturalDocsTestScript : MonoBehaviour
+{
+    // EX-1: SIMPLE
+    /////////////////////////////////
+    // Function: DoFunStuff
+    // Like dance & stuff!
+    public int DoFunStuff(int x, string b)
+    {
+        return 39;
+    }
+
+    // EX-2: ADVANCED
+    /////////////////////////////////
+    /* Function: Multiply
+
+    Multiplies two integers.
+
+    Parameters:
+
+        x - The first integer.
+        y - The second integer.
+
+    Returns:
+
+        The two integers multiplied together.
+
+    See Also:
+
+        <DoFunStuff>
+    */
+    private int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+}
